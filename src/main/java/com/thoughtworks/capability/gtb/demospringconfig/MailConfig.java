ab93f07219243a0be13da37199a591eeba7fd7a8
lemon-piepie <purplechilde@163.com>
2020-09-18 20:47:50 +0800

    p1s1

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,18 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+import java.util.Map;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private List defaultRecipients;
+    private Map additionalHeaders;
+    private Credentials credentials;
 
     public String getHostname() {
         return hostname;
@@ -39,6 +45,34 @@ public class MailConfig {
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
+                ", defaultRecipients=" + defaultRecipients +
+                ", additionalHeaders=" + additionalHeaders +
+                ", credentials=" + " username: " + credentials.getUsername()
+                + " password: " + credentials.getPassword() + " authMethod: " + credentials.getAuthMethod() +
                 '}';
+    }
+
+    public List getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
+    public Map getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
+    public Credentials getCredentials() {
+        return credentials;
+    }
+
+    public void setCredentials(Credentials credentials) {
+        this.credentials = credentials;
     }
 }
