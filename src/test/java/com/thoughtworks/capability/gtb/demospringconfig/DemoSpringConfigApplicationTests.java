5fcf0f1d9cd199dad1efd8d115257470b94fa9c6
lemon-piepie <purplechilde@163.com>
2020-09-18 21:01:04 +0800

    p2s1

--- src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
+++ src/test/java/com/thoughtworks/capability/gtb/demospringconfig/DemoSpringConfigApplicationTests.java
@@ -1,13 +1,46 @@
 package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class DemoSpringConfigApplicationTests {
+	@Autowired
+	private LevelController levelController;
 
 	@Test
 	void contextLoads() {
+		assertEquals("basic",levelController.getLevel());
+	}
+
+}
+
+@SpringBootTest
+@ActiveProfiles("test2")
+class DemoSpringConfigApplicationTests2 {
+	@Autowired
+	private LevelController levelController;
+
+	@Test
+	void contextLoads() {
+		assertEquals("advanced",levelController.getLevel());
+	}
+
+}
+
+@SpringBootTest
+@ActiveProfiles("test3")
+class DemoSpringConfigApplicationTests3 {
+	@Autowired
+	private LevelController levelController;
+
+	@Test
+	void contextLoads() {
+		assertEquals("advanced",levelController.getLevel());
 	}
 
 }
